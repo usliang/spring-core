b87a628ef60d12396134727ae51ab916a7ed6bcd
liliang <6568959+usliang@users.noreply.github.com>
2025-05-13 23:15:40 -0700

    end 05/15/2025

--- src/main/java/com/liang/springcore/leetcode/StrMultiplyProcessor.java
+++ src/main/java/com/liang/springcore/leetcode/StrMultiplyProcessor.java
@@ -17,8 +17,11 @@ public class StrMultiplyProcessor {
         ArrayList<Integer> answer = new ArrayList<>(results.get(results.size() - 1));
         for (int i = 0; i < results.size() - 1; i++) {
            int carry = 0;
+           ArrayList<Integer> current = new ArrayList<>(results.get(i));
            for (int j = 0; j<answer.size() || j< results.get(i).size(); j++) {
-
+                int digit1 = j<answer.size() ? answer.get(j) : 0;
+                int digit2 = j< results.get(i).size() ? results.get(i).get(j) : 0;
+                int sum = digit1 + digit2 + carry;
            }
         }
 
