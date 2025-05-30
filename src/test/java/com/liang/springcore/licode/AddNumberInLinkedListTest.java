102c278cfb75dc4c60132981eddbc87177589685
liliang <6568959+usliang@users.noreply.github.com>
2025-05-30 09:12:31 -0700

    add test case

--- src/test/java/com/liang/springcore/licode/AddNumberInLinkedListTest.java
+++ src/test/java/com/liang/springcore/licode/AddNumberInLinkedListTest.java
@@ -13,11 +13,44 @@ class AddNumberInLinkedListTest {
     void swapPairs() {
         AddNumberInLinkedList list = new AddNumberInLinkedList();
         AddNumberInLinkedList.ListNode head = new AddNumberInLinkedList.ListNode(1);
-        AddNumberInLinkedList.ListNode rest = new AddNumberInLinkedList.ListNode(2);
-        head.next = rest;
+        head.next = new AddNumberInLinkedList.ListNode(2);
 
         AddNumberInLinkedList.ListNode newHead = list.swapPairs(head);
         assertEquals(2, newHead.val);
         assertEquals(1, newHead.next.val);
+
+        //test 3 nodes
+        newHead.next.next = new AddNumberInLinkedList.ListNode(3);
+        head = list.swapPairs(newHead);
+        assertEquals(1, head.val);
+        assertEquals(2, head.next.val);
+        assertEquals(3, head.next.next.val);
+
+        //test 4 nodes
+        head.next.next.next = new AddNumberInLinkedList.ListNode(4);
+        head = list.swapPairs(head);
+        assertEquals(2, head.val);
+        assertEquals(1, head.next.val);
+        assertEquals(4, head.next.next.val);
+        assertEquals(3, head.next.next.next.val);
+
+        //test 5 nodes
+        head.next.next.next.next = new AddNumberInLinkedList.ListNode(5);
+        head = list.swapPairs(head);
+        assertEquals(1, head.val);
+        assertEquals(2, head.next.val);
+        assertEquals(3, head.next.next.val);
+        assertEquals(4, head.next.next.next.val);
+        assertEquals(5, head.next.next.next.next.val);
+
+        //test 6 nodes
+        head.next.next.next.next.next = new AddNumberInLinkedList.ListNode(6);
+        head = list.swapPairs(head);
+        assertEquals(1, head.val);
+        assertEquals(2, head.next.val);
+        assertEquals(3, head.next.next.val);
+        assertEquals(4, head.next.next.next.val);
+        assertEquals(5, head.next.next.next.next.val);
+        assertEquals(5, head.next.next.next.next.next.val);
     }
 }
